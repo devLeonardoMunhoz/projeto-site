7ac2a1921f6d82f8857b093f02f5dfdcc7923cce
Leonardo Munhoz <munhoz.leonardo304@gmail.com>
2022-08-31 22:29:18 -0300

    aulas 10 e 11

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 73
+console.log (`A velocidade do seu carro foi ${vel} km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade MULTADO!!')
+}
+
+console.log('Dirija sempre usando o sinto de segurança')
